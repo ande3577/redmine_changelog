8cfcb83e60ffa34ea825177a56dcfa0dfec47c04
ande3577 <ande3577@gmail.com>
2014-10-26 09:57:11 -0500

    Add support for specifying start, end version

--- init.rb
+++ init.rb
@@ -13,19 +13,33 @@ Redmine::WikiFormatting::Macros.macro :changelog_version, :desc => 'Wiki macro t
     end
     
     version = Version.where(:name => version_name).first
-    render :partial => 'changelog/changelog_version', :locals => { :version => version, :project => @project, :link_to_issues => link_to_issues }
+    render :partial => 'changelog/changelog_version', :locals => { :version => version, :link_to_issues => link_to_issues }
   end
 
   Redmine::WikiFormatting::Macros.macro :changelog, :desc => 'Wiki macro to generate a changelog
 
   {{changelog()}}: generates a changeset showing issues closed on a given target revision.
-  {{changelog(false)}}: do not include the issue number/link in the listing' do |obj, args|
+  {{changelog(false)}}: do not include the issue number/link in the listing
+  {{changelog(true, start_version_name, end_version_name)}}: limit the changlog to
+    versions with a target date between the start and end version.  If the start/end
+    version name is invalid or the version does not have an effective date, the 
+    limit check will be ignored. Versions without a specified target date will not be 
+    shown in the changelog.
+' do |obj, args|
     link_to_issues = true
-    if args and !args.empty? and (args[0].casecmp('false') == 0)
-      link_to_issues = false
+    start_version_name = nil
+    end_version_name = nil
+    if args 
+      link_to_issues = false if args.size >= 1 and (args[0].strip.casecmp('false') == 0)
+      start_version_name = args[1] if args.size >= 2
+      end_version_name = args[2] if args.size >= 3
     end
-    
-    render :partial => 'changelog/changelog', :locals => { :project => @project, :link_to_issues => link_to_issues }
+    versions = @project.versions.sort
+    render :partial => 'changelog/changelog', :locals => { :project => @project, 
+      :versions => versions, 
+      :link_to_issues => link_to_issues, 
+      :start_version_name => start_version_name, 
+      :end_version_name => end_version_name }
   end
   
   name 'Redmine Changelog plugin'
--- lib/changelog_application_helper_patch.rb
+++ lib/changelog_application_helper_patch.rb
@@ -5,6 +5,34 @@ module ChangelogApplicationHelperPatch
     base.extend(ClassMethods)
     base.send(:include, InstanceMethods)
     base.class_eval do
+      def changelog(project, link_to_issues, start_version_name, end_version_name)
+        html = ""
+        versions = project.versions
+
+        start_version = start_version_name.nil? ? nil : versions.where(:name => start_version_name).first
+        start_date = start_version ? start_version.effective_date : nil
+        versions.select! { |v| v.effective_date and v.effective_date >= start_date } if start_date
+
+        end_version = end_version_name.nil? ? nil : versions.where(:name => end_version_name).first
+        end_date = end_version ? end_version.effective_date : nil
+        versions.select! { |v| v.effective_date and v.effective_date <= end_date } if end_date
+
+        versions.sort { |x, y|
+          # force versions without effective date to end of list
+          if !x.effective_date and y.effective_date
+            1
+          elsif !y.effective_date and x.effective_date
+            -1
+          else
+            # otherwise reverse to show newest revision first
+            y <=> x 
+          end 
+        }.each do |v|
+          html << changelog_version(v, link_to_issues)
+        end
+        html.html_safe
+      end
+
       def changelog_version(v, link_to_issues) 
         html = ""
         html << "<h3>"
